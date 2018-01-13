9db0ae6bd8dbc2aa077775bf4321a51b5f9952e3
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-01-13 16:51:41 +0000

    ConferenceRoom 1st test passed

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,2 +1,15 @@
-public class ConferenceRoom {
+public class ConferenceRoom extends Room {
+
+    private String name;
+    private double rate;
+
+    public ConferenceRoom(int capacity, String name, double rate){
+        super(capacity);
+        this.name = name;
+        this.rate = rate;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
---
+++ src/test/java/ConferenceRoomTest.java
@@ -0,0 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConferenceRoomTest {
+
+    ConferenceRoom conferenceRoom;
+
+    @Before
+
+    public void before(){
+        conferenceRoom = new ConferenceRoom(15, "Sun Room", 90.0);
+    }
+
+    @Test
+
+    public void canGetName(){
+        assertEquals("Sun Room", conferenceRoom.getName());
+
+    }
+}
